b08b2dc8bd8133e9e4d3512a17379fa64bb07882
kostisalexas <konstantinosa57@gmail.com>
2025-05-21 17:05:26 +0300

    I just contributed to the code!

--- app/src/test/java/com/example/fitguide/ExampleUnitTest.kt
+++ app/src/test/java/com/example/fitguide/ExampleUnitTest.kt
@@ -8,6 +8,8 @@ import org.junit.Assert.*
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
+ 
+ I just contributed to the code!
  */
 class ExampleUnitTest {
     @Test
